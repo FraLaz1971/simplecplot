271f672d9fd8b6f5b659a1777dadfb367b344c7c
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-11-19 16:45:33 +0100

    updated analysis.c new correct version

--- src/analysis.c
+++ src/analysis.c
@@ -1,48 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fun/functions.h" 
+#include "fun/data.h" 
 
-int main(int argc, char **argv)
+int main()
 {
-    if (argc == 4){
-	puts("Hello Analysis");
-    if (argc == 4){
-    nrows=atoi(argv[2]); ncols=(atoi(argv[3]));
-    	/* allocate a mybuffer of chars with length bufsize */
-    memcpy(infile, argv[1], strlen(argv[1]));
-
-    open_data_file(infile);
-
-    allocate_buffer();
-
-    allocate_data();
-
-    parse_data_file();
-
-    showdata(data, nrows, ncols); 
-    
-/*    create_data_file("temp.tsv"); */
-    /* */
-/*    gp_plot_screen("ascii2screen.gp"); */
-/*    gp_plot_png("test.gp"); */
-//    pu_graph("ciccio.gp"); 
-    deallocate_buffer();
-
-    deallocate_data();
-
-    return 0;
- 
-    } else {
-    fprintf(stderr, "usage: %s infilename nrows ncolumns\n", argv[0]);
-	return 1;
+        struct MetaDataMx2 m2d;    struct DataMx2 d2d;
+        simple_data(&m2d, &d2d);
+        return 0;        
 }
 
-    return 0;
- 
-    } else {
-    printf("usage: %s infilename nrows ncolumns\n", argv[0]);
-	return 1;
-}
-
-
-}
